af2a7aa150d1a92f6ce09fd38290c8d6d0807eee
奥田　智宏 <meganenimeganai@gmail.com>
2017-04-27 12:34:57 +0900

    Update peint3.php

--- peint3.php
+++ peint3.php
@@ -16,7 +16,7 @@ background-size:100% 100%;
     <?
         try {
  
-$connectionInfo = array("UID" => "i13008@oktm", "pwd" => "12345678a!", "Database" => "i13008", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "i13008@oktm", "pwd" => "*パスワードなので秘匿*", "Database" => "i13008", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:oktm.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
